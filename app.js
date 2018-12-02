8bb35d51755f84a2ed9710bf1821a55e097c0088
daclouds <daclouds@gmail.com>
2018-12-02 10:37:49 +0900

    get random data when server is down for testing purpose

--- app.js
+++ app.js
@@ -38,14 +38,19 @@ let job;
 const updateUse = () => {
   return request('http://192.168.0.105', (error, response, body) => {
     console.log(body);
-    const { RESTROOM: [ lastModified, id, use, acc, usePercent ]} = JSON.parse(body);
-    if (lastSync && lastSync >= lastModified) {
-      return;
+    if (!body) return updateRandomUse();
+    try {
+      const { RESTROOM: [ lastModified, id, use, acc, usePercent ]} = JSON.parse(body);
+      if (lastSync && lastSync >= lastModified) {
+        return;
+      }
+      io.emit('message', JSON.stringify({
+        use: usePercent >= 100 ? 0 : use,
+        rest: usePercent >= 100 ? 100 : 100 - usePercent,
+      }));
+    } catch (e) {
+      return updateRandomUse()
     }
-    io.emit('message', JSON.stringify({
-      use,
-      rest: 100 - usePercent,
-    }));
   });
 }
 
